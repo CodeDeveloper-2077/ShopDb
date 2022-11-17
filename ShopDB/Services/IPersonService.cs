28beb1fe986894ef64a8c528a5c4db4666ad6fb5
Oleh Sh <oleh.shevchenko.02@gmail.com>
2022-11-17 10:00:34 +0100

    Configuration of PersonService

---
+++ ShopDB/Services/IPersonService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDB.Services
+{
+    public interface IPersonService
+    {
+        Person AddPerson(int id, string name, string surname);
+        Person GetPerson(Func<Person, bool> predicate);
+        Task<Person> GetPersonAsync(Expression<Func<Person, bool>> expression);
+        List<Person> GetSortedPeople();
+        Task<List<Person>> GetSortedPeopleAsync();
+        IEnumerable<Person> GetPeopleOlderThanDate(DateTime date);
+        Task<IEnumerable<Person>> GetPeopleOlderThanDateAsync(object state);
+    }
+}
---
+++ ShopDB/Services/PersonService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ShopDB.Services
+{
+    public class PersonService : IPersonService
+    {
+        private readonly ShopDBContext _context;
+
+        public PersonService(ShopDBContext context)
+        {
+            this._context = context;
+        }
+
+        public Person AddPerson(int id, string name, string surname)
+        {
+            if (name == string.Empty || surname == string.Empty)
+                throw new ArgumentNullException($"Provide {nameof(name)} and {nameof(surname)} please!");
+
+            var person = _context.People.Add(new Person
+            {
+                Id = id,
+                Name = name,
+                Surname = surname
+            });
+            _context.SaveChanges();
+
+            return person;
+        }
+
+        public List<Person> GetSortedPeople()
+        {
+            var people = from person in _context.People
+                         orderby person.Name descending
+                         select person;
+
+            return people.ToList();
+        }
+
+        public async Task<List<Person>> GetSortedPeopleAsync()
+        {
+            var people = from person in _context.People
+                         orderby person.Name descending
+                         select person;
+
+            return await people.ToListAsync();
+        }
+
+        public IEnumerable<Person> GetPeopleOlderThanDate(DateTime date)
+        {
+            return _context.People
+                                    .Where(p => p.BirthDate.Year > date.Year)
+                                    .ToList();
+        }
+
+        public async Task<IEnumerable<Person>> GetPeopleOlderThanDateAsync(object state)
+        {
+            DateTime date = (DateTime)state;
+            return await _context.People
+                                    .Where(p => p.BirthDate.Year > date.Year)
+                                    .ToListAsync();
+        }
+
+        public Person GetPerson(Func<Person, bool> predicate)
+        {
+            return _context.People.FirstOrDefault(predicate);
+        }
+
+        public async Task<Person> GetPersonAsync(Expression<Func<Person, bool>> expression)
+        {
+            return await _context.People.FirstOrDefaultAsync(expression);
+        }
+    }
+}
